8ec2ae7505f4f76d5f91d0c335591caf8e16c27f
knikolov <kami_985@abv.bg>
2022-01-11 15:21:08 +0200

    Use state and use Effect hooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [counter,setCounter] = useState(1);
+  useEffect(()=>{
+
+    let titleText = "Count: "+ counter;
+    document.title = titleText;
+  },[counter]);
   return (
     <div className="App">
       <section class="hero">
         <div class="hero-body">
-          <p class="title">A React Task</p>
+          <p class="title"></p>
           <p class="subtitle">by Boom.dev</p>
         </div>
       </section>
       <div class="container is-fullhd">
         <div class="notification">
           Edit the <code>./src</code> folder to add components.
+        </div>
+        <div>
+          <button 
+          onClick= {()=>{
+                          setCounter(
+                            (counter)=>{return counter+1;
+                                        });
+                        }
+                    }
+          
+          >Count: {counter}</button>
         </div>
       </div>
     </div>
